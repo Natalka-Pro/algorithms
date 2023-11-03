794a7c7a0cd30b20fb97a1417ddd5fd7f8afd61d
NatalkaPron <natalka-pronina@mail.ru>
2023-11-03 14:17:31 +0300

    Started tasks for the 1th lecture

---
+++ D. Уравнение с корнем.py
@@ -0,0 +1,72 @@
+"""
+Решите в целых числах уравнение:
+sqrt(ax + b) = c,
+a, b, c – данные целые числа: найдите все решения или сообщите, что решений в целых 
+числах нет. 
+
+Формат ввода
+Вводятся три числа a, b и c по одному в строке.
+
+Формат вывода
+Программа должна вывести все решения уравнения в порядке возрастания, либо NO SOLUTION 
+(заглавными буквами), если решений нет. Если решений бесконечно много, вывести MANY 
+SOLUTIONS. 
+
+Пример 1
+Ввод
+Вывод
+1
+0
+0
+
+0
+
+Пример 2
+Ввод
+Вывод
+1
+2
+3
+
+7
+
+Пример 3
+Ввод
+Вывод
+1
+2
+-3
+
+NO SOLUTION
+"""
+
+a, b, c = int(input()), int(input()), int(input())
+ans = None
+
+if c < 0:
+    ans = "NO SOLUTION"
+elif c == 0:
+    if a == 0:
+        if b != 0:
+            ans = "NO SOLUTION"
+        else:
+            ans = "MANY SOLUTIONS"
+    else:
+        ans = -b/a
+else: # ax + b = c^2
+    if a == 0:
+        if b != c**2:
+            ans = "NO SOLUTION"
+        else:
+            ans = "MANY SOLUTIONS"
+    else:
+       ans = (c**2 - b) / a
+
+
+if type(ans) is float:
+    if int(ans) == ans:
+        print(int(ans))
+    else:
+        print("NO SOLUTION")
+else:
+    print(ans)
---
+++ testing.py
@@ -0,0 +1,63 @@
+# import re
+
+def find_all_substrings(string, substring):
+    # Initialize an empty list to store the 
+    # indices of all occurrences of the substring.
+    indices = []
+    # Set the starting index i to 0.
+    i = 0
+    # Use a while loop to keep searching for 
+    # the substring in the string.
+    while i < len(string):
+        # Use the find() method to find the first 
+        #occurrence of the substring in the string
+        j = string.find(substring, i)
+        # If find() returns -1, it means that there  
+        # are no more occurrences of the substring in 
+        # the string, so break out of the loop.
+        if j == -1:
+            break
+        indices.append(j)
+        i = j + len(substring)
+    # Return the list of indices.
+    return indices
+
+
+f = open('input.txt', 'r')
+text = f.read().strip()
+
+text_new = text.replace("\n\n", "\n")
+while text_new != text:
+    text = text_new
+    text_new = text_new.replace("\n\n", "\n")
+
+
+s = "Формат ввода", "Формат вывода", "Пример", "Примечания"
+
+for i in s:
+    if text.find(i) != -1:
+        # idx = [m.start() for m in re.finditer(i, text)]
+        idx = find_all_substrings(text, i)
+
+        for x in idx[::-1]: # если вставлять сначала, то индексы мест вставки меняются
+            text = text[:x] + "\n" + text[x:]
+
+ans = ""
+for string in text.split("\n"):
+    string = string.strip() # чтобы табуляцию заменить на \n
+    if len(string) < 80:
+        ans += string + "\n"
+    else:
+        words = string.split(" ")
+        string_new = ""
+        for word in words:
+            if len(string_new) < 80:
+                string_new += word + " "
+            else:
+                ans += string_new + "\n"
+                string_new = word + " "
+
+        ans += string_new + "\n"
+
+f = open('output.txt', 'w')
+f.write(ans)
